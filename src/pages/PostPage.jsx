3e42cdb084ac3952adc235276eabbcd19ffa72b2
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2024-02-19 15:00:01 +0200

    fetching data from the server

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,28 +2,24 @@ import { useEffect, useState } from 'react';
 import pageClasses from './Page.module.css';
 import { useParams } from 'react-router-dom';
 
-const dummyData = [
-    {id:"_ONE", title:'title one', description:'description one'},
-    {id:"_TWO", title:'title two', description:'description two'},
-    {id:"_THREE", title:'title three', description:'description three'},
-];
-
-
 const PostPage = () => {
-    const [data, setData] = useState({title:null, description:null});
+    const [post, setPost] = useState(null);
     const { id } = useParams();
 
     useEffect(()=>{
-        // console.log(id);
-        const [post] = dummyData.filter((post)=>{
-            return post.id === id;
-        });
-        setData({title: post.title, description: post.description});
-    }, [id])
+        const fetchPosts = async ()=>{
+            const response = await fetch('http://localhost:4000/api/posts/'+id);
+            const json = await response.json();
+            if(response.ok) {
+                setPost(json);
+            }
+        }
+        fetchPosts();
+    }, [id]);
 
     return ( <div className={pageClasses['page']}>
-        <h1>{ data && data.title }</h1>
-        <h1>{ data && data.description }</h1>
+        <h1>{ post && post.title }</h1>
+        <h1>{ post && post.description }</h1>
     </div> );
 }
  
