c89322d4efc922d1fca3a3ca0b0318a3b7e3f842
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2024-02-20 14:48:56 +0200

    posting new posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,35 @@
 import pageClasses from './Page.module.css';
 import PostsList from './post/PostsList';
 import { useEffect, useState } from "react";
+import FloatingButton from '../components/FloatingButton';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const [posts, setPosts] = useState(null);
+    const navigate = useNavigate();
     useEffect(()=>{
-        const fetchPosts = async ()=>{
-            const response = await fetch('http://localhost:4000/api/posts/');
-            const json = await response.json();
-            if(response.ok) {
-                setPosts(json);
+        const fetchPosts = async () => {
+            try {
+              const response = await fetch('http://localhost:4000/api/posts/');
+              if (!response.ok) {
+                throw new Error('Failed to fetch data');
+              }
+              const json = await response.json();
+              setPosts(json);
+            } catch (error) {
+              console.error('Error fetching data:', error.message);
             }
-        }
+          };
         fetchPosts();
     }, []);
 
+    const postFormHandler = ()=> {
+        navigate('/post-form');
+    }
+
     return ( <div className={`${pageClasses['page']} ${pageClasses['home']}`}>
         {posts && <PostsList data={posts}/>}
+        <FloatingButton onClick={ postFormHandler }/>
     </div> );
 }
 
